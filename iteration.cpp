0e1336e900b2165ba0e38af59d20c272c830fd10
Vikas Yadav <vikashyadav62502@gmail.com>
2023-06-02 13:22:43 +0530

    Initial commit

---
+++ iteration.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int binarySearch(int arr, int s, int e, int n){
+    int mid = (s+e)/2;
+
+    while(s<e){
+        if(){
+
+        }
+    }
+}
+
+
+int main(){
+    int arr[] = {1,2,3,4,5};
+
+    int n = 5;
+
+    return 0;
+}
