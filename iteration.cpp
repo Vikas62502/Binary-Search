82310b2070072e9f06e00afb551c92a99e2c6ffe
Vikas Yadav <vikashyadav62502@gmail.com>
2023-06-03 23:53:35 +0530

    Recursive Way of iterating Binary search

--- iteration.cpp
+++ iteration.cpp
@@ -1,12 +1,18 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int binarySearch(int arr, int s, int e, int n){
+int binarySearch(int arr[], int s, int e, int k){
     int mid = (s+e)/2;
 
     while(s<e){
-        if(){
-
+        if(arr[mid]==k){
+            return mid;
+        }
+        else if(arr[mid]<k){
+            s = mid+1;
+        }
+        else{
+            e = mid-1;
         }
     }
 }
@@ -16,6 +22,6 @@ int main(){
     int arr[] = {1,2,3,4,5};
 
     int n = 5;
-
+  cout<<binarySearch(arr,0,5,5);
     return 0;
 }
